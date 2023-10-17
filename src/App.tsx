53f35668714736764707c308cd0a422dd5c5c9cc
Zaber Ahmed <zaber0305@gmail.com>
2023-10-17 11:42:21 +0600

    Add: tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return <div className=" text-[#28282B]">Hello zaber</div>;
+}
+
+export default App;
